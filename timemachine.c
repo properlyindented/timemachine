55a2620a554b1552b6f0fb16c8e5d40d35b68295
Robert Martin-Legene <robert@martin-legene.dk>
2017-11-13 01:23:08 -0300

    Streamlining, preparing for new feature.

--- timemachine.c
+++ timemachine.c
@@ -1,9 +1,9 @@
 /*
-   To compile:
-    gcc -fPIC -shared -o libtimemachine.so timemachine.c -ldl
-   To use:
-    SECONDS_OFFSET=14123 LD_PRELOAD=`pwd`/libtimemachine.so date
-
+    20150325 Robert Martin-Legene
+    To compile:
+      gcc -fPIC -shared -o libtimemachine.so timemachine.c -ldl
+    To use:
+      SECONDS_OFFSET=14123 LD_PRELOAD=`pwd`/libtimemachine.so date
  */
 
 #define _GNU_SOURCE
@@ -13,43 +13,49 @@
 
 static  time_t  (*real_time)(time_t *t) = NULL;
 static  int     (*real_clock_gettime)(clockid_t clk_id, struct timespec *tp) = NULL;
-static  long    *offz    = NULL;
+static  long    timemachine_offz    = 0;
+static  int     timemachine_initialised = 0;
 
-long    offset()
+void    timemachine_init()
 {
-    if ( ! offz )
-    {
-        offz = (long*) malloc( sizeof(long) );
-        *offz = 0;
-        char *var = getenv("SECONDS_OFFSET");
-        if ( var )
-            *offz = atol( var );
-    }
-    return *offz;
+    char *timemachine_secs_offset;
+    if ( timemachine_initialised )
+        return;
+    timemachine_initialised = 1;
+
+    timemachine_secs_offset = getenv("SECONDS_OFFSET");
+    if ( timemachine_secs_offset )
+        timemachine_offz = atol( timemachine_secs_offset );
+
+    if ( ! real_time )
+        real_time           =   dlsym( RTLD_NEXT, "time" );
+    if ( ! real_clock_gettime )
+        real_clock_gettime  =   dlsym( RTLD_NEXT, "clock_gettime" );
 }
 
 time_t time(time_t *t)
 {
-    if ( ! real_time ) /* Only look up once */
-        real_time       =   dlsym( RTLD_NEXT, "time" );
+    time_t  res;
+    timemachine_init();
     if ( ! real_time ) /* if we couldn't find it */
         return -1;
-    /* call the real function */
-    time_t  res         =   real_time(t);
     if ( t != NULL )
-        t += offset();
-    res += offset();
-    return res;
+        t += timemachine_offz;
+    /* call the real function */
+    res =  real_time(t);
+    return (res + timemachine_offz);
 }
 
 int clock_gettime(clockid_t clk_id, struct timespec *tp)
 {
-    if ( ! real_clock_gettime ) /* Only look up once */
-        real_clock_gettime       =   dlsym( RTLD_NEXT, "clock_gettime" );
-    if ( ! real_clock_gettime ) /* if we couldn't find it */
+    timemachine_init();
+    /* if we couldn't find it */
+    if ( ! real_clock_gettime )
         return -1;
     int rv = real_clock_gettime( clk_id, tp );
+    if (rv != 0)
+        return rv;
     if (rv == 0)
-        tp->tv_sec += offset();
+    tp->tv_sec += timemachine_offz;
     return rv;
 }
